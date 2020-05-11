afc926840398e7ab1d82cb849ad642abdde729e1
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2020-05-11 17:51:06 -0500

    initial commit

---
+++ LessWaste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LessWaste
+//
+//  Created by Héctor David Hernández Rodríguez on 23/04/20.
+//  Copyright © 2020 Héctor David Hernández Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '13.0'
+
+target 'LessWaste' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LessWaste
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+
+end
