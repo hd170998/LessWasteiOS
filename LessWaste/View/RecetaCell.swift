47753aece96ae7ffe68aef478edb532ceee0c97c
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2020-05-12 01:22:33 -0500

    table view

---
+++ LessWaste/View/RecetaCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecetaCell.swift
+//  LessWaste
+//
+//  Created by Héctor David Hernández Rodríguez on 12/05/20.
+//  Copyright © 2020 Héctor David Hernández Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class RecetaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
