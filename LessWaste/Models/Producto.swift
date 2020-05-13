a4ee92c27f6c2295d5b1c67bccf3e28ded864a15
Marlon <a01379404@itesm.mx>
2020-05-13 02:01:24 -0400

    avance 1

---
+++ LessWaste/Models/Producto.swift
@@ -0,0 +1,13 @@
+//
+//  Producto.swift
+//  LessWaste
+//
+//  Created by user168972 on 5/13/20.
+//  Copyright © 2020 Héctor David Hernández Rodríguez. All rights reserved.
+//
+
+import Foundation
+struct Producto {
+    let nombre: String
+    let caducidad: String
+}
